b751d6637536b6066a0e02f834dc5c72d9715429
majdjb7 <majdjd171@gmail.com>
2022-01-19 15:41:49 +0200

    add all data ready 100%

--- insertData.js
+++ insertData.js
@@ -16,7 +16,6 @@ const addTowns = async function() {
             })
     })
 }
-// addTowns()
 
 const addTypes = async function() {
     let typesList = []
@@ -33,8 +32,6 @@ const addTypes = async function() {
     })
 }
 
-// addTypes()
-
 const addPokemon = async function() {
     pokemonsData.forEach(async p=> {
         let poke_type = p.type
@@ -50,23 +47,55 @@ const addPokemon = async function() {
         return newResult[0]
     })
 }
-// addPokemon()
 
 const addTrainer = async function() {
+    let trainersList = []
     pokemonsData.forEach(p=> {
         p.ownedBy.map(async o => {
-            let name = o.name
-            let townName = o.town
-            let townQuery = `SELECT tn_id FROM town WHERE t_name='${townName}'`
-            let result = await sequelize.query(townQuery)
-            let town_id = result[0][0].tn_id
-            let trainerQuery = `INSERT INTO trainer VALUES (null, '${name}', ${town_id})`;
-            let newResult = await sequelize.query(trainerQuery)
-            return newResult[0]
+            if(!trainersList.includes(o.name)) {
+                trainersList.push(o.name)
+                let name = o.name
+                let townName = o.town
+                let townQuery = `SELECT tn_id FROM town WHERE t_name='${townName}'`
+                let result = await sequelize.query(townQuery)
+                let town_id = result[0][0].tn_id
+                let trainerQuery = `INSERT INTO trainer VALUES (null, '${name}', ${town_id})`;
+                let newResult = await sequelize.query(trainerQuery)
+                return newResult[0]
+            }
         })
     }) 
     }
-// addTrainer()
+
+const linkPokemonWithTrainers = async function() {
+    pokemonsData.forEach(p=> {
+        let poke_id = p.id
+        p.ownedBy.map(async o => {
+            let owner = o.name
+            let ownerQuery = `SELECT tr_id FROM trainer WHERE t_name='${owner}'`
+            let result = await sequelize.query(ownerQuery)
+            let owner_ids = result[0]
+            console.log(owner_ids)
+            // owner_ids.forEach(async o_id => {
+                
+            // })
+            let pokemonAndTrainerQuery = `INSERT INTO pokemon_trainer VALUES ('${poke_id}', ${owner_ids[0].tr_id})`;
+            let newResult = await sequelize.query(pokemonAndTrainerQuery)
+            return newResult[0]  
+        })
+    })
+}
+
+function addAllData() {
+    addTowns()
+        .then(addTrainer())
+    addTypes()
+        .then(addPokemon())
+}
+
+// addAllData()
+//     .then(linkPokemonWithTrainers())
+
 
 sequelize
     .authenticate()
